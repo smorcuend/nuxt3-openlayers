62df724f437c1437dbd3e8c765b19d45efc8bdf5
Sergio <sergio.morcuende@cesefor.com>
2022-03-23 15:07:42 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height: 400px">
+    <ol-view
+      ref="view"
+      :center="center"
+      :rotation="rotation"
+      :zoom="zoom"
+      :projection="projection"
+    />
+
+    <ol-tile-layer>
+      <ol-source-osm />
+    </ol-tile-layer>
+  </ol-map>
+</template>
+
+<script setup>
+const center = ref([40, 40]);
+const projection = ref("EPSG:4326");
+const zoom = ref(8);
+const rotation = ref(0);
+</script>
